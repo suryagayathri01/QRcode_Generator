2da24e8b36e7feb8bf0c3484a1530ae098f8fd88
Suryagayathri K P <suryagayathrikp@gmail.com>
2023-09-06 23:22:10 +0530

    removed format

---
+++ qrcode_gen/index.js
@@ -0,0 +1,21 @@
+const slider = document.getElementById("quality");
+const sliderValue = document.getElementById("qualityValue");
+const btn = document.getElementById("generatebtn");
+
+slider.addEventListener("input", () => {
+	sliderValue.textContent = slider.value;
+});
+
+const generate = (e) => {
+	e.preventDefault();
+	const qrdata = document.getElementById("content").value;
+	const resolution = document.getElementById("quality").value;
+
+	const url = `https://api.qrserver.com/v1/create-qr-code/?data=${qrdata}&size=${resolution}x${resolution}&format=${format}`;
+
+	document.getElementById("image-container").src = url;
+};
+
+btn.addEventListener("click", (e) => {
+	generate(e);
+});
