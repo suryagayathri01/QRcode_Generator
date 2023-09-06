c032e8ff4e499b39c95acb9804d81f08fc815662
Suryagayathri K P <suryagayathrikp@gmail.com>
2023-09-06 23:41:39 +0530

    bg color

--- qrcode_gen/index.js
+++ qrcode_gen/index.js
@@ -9,6 +9,7 @@ slider.addEventListener("input", () => {
 const generate = (e) => {
 	e.preventDefault();
 	const qrdata = document.getElementById("content").value;
+	const format = document.getElementById("format").value;
 	const resolution = document.getElementById("quality").value;
 
 	const url = `https://api.qrserver.com/v1/create-qr-code/?data=${qrdata}&size=${resolution}x${resolution}&format=${format}`;
