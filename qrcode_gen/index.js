50b66ac9ced087a0da9469e6d67cb19cafce78b6
Suryagayathri K P <suryagayathrikp@gmail.com>
2023-09-28 15:23:11 +0530

    removed slider

--- qrcode_gen/index.js
+++ qrcode_gen/index.js
@@ -1,17 +1,11 @@
-const slider = document.getElementById("quality");
-const sliderValue = document.getElementById("qualityValue");
 const btn = document.getElementById("generatebtn");
 
-slider.addEventListener("input", () => {
-	sliderValue.textContent = slider.value;
-});
 
 const generate = (e) => {
 	e.preventDefault();
 	const qrdata = document.getElementById("content").value;
-	const resolution = document.getElementById("quality").value;
 
-	const url = `https://api.qrserver.com/v1/create-qr-code/?data=${qrdata}&size=${resolution}x${resolution}`;
+	const url = `https://api.qrserver.com/v1/create-qr-code/?data=${qrdata}&size=250*250`;
 
 	document.getElementById("image-container").src = url;
 };
