bb60ddd62a2d88b9a196e6fe83e816c76747e4cf
Suryagayathri K P <suryagayathrikp@gmail.com>
2024-01-01 18:55:39 +0530

    minor changes

--- qrcode_gen/index.js
+++ qrcode_gen/index.js
@@ -1,15 +1,15 @@
 const btn = document.getElementById("generatebtn");
 
-
 const generate = (e) => {
-	e.preventDefault();
-	const qrdata = document.getElementById("content").value;
+  e.preventDefault();
+  const qrdata = document.getElementById("content").value;
 
-	const url = `https://api.qrserver.com/v1/create-qr-code/?data=${qrdata}&size=250*250`;
+  const url = `https://api.qrserver.com/v1/create-qr-code/?data=${qrdata}&size=250*250`;
 
-	document.getElementById("image-container").src = url;
+  document.getElementById("image-container").src = url;
+  document.getElementById("image-container").style.visibility = "visible";
 };
 
 btn.addEventListener("click", (e) => {
-	generate(e);
+  generate(e);
 });
